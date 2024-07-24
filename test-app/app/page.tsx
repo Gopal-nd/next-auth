b4bc612179913994b04fda2033819de9c908af51
Gopal <gopalhelpyou@gmail.com>
2024-07-24 19:31:17 +0530

    razerpay

---
+++ test-app/app/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+import AnimationControle from '@/components/AnimationControle'
+import Button from '@/components/Button'
+import Scroll from '@/components/Scroll'
+import WhileInView from '@/components/WhileInView'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+ {/* <Button /> */}
+ {/* <AnimationControle /> */}
+ {/* <WhileInView /> */}
+ <Scroll />
+
+    </div>
+  )
+}
+
+export default Home
